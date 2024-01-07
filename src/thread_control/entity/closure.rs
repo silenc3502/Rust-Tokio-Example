981a4ae58c3921c30573b9802f6157f4c6e84e73
silenc3502 <gcccompil3r@gmail.com>
2024-01-07 12:25:26 +0900

    update: finish to update thread control

--- src/thread_control/entity/closure.rs
+++ src/thread_control/entity/closure.rs
@@ -1,7 +1,12 @@
 use std::future::Future;
 use std::pin::Pin;
 
+// pub enum Closure {
+//     Async(Box<dyn FnMut() -> Pin<Box<dyn Future<Output = ()> + Send>>>),
+//     Sync(Box<dyn FnMut() -> ()>),
+// }
+
 pub enum Closure {
-    Async(Box<dyn FnMut() -> Pin<Box<dyn Future<Output = ()> + Send>>>),
-    Sync(Box<dyn FnMut() -> ()>),
+    Async(Box<dyn FnOnce() -> Pin<Box<dyn Future<Output = ()> + Send>>>),
+    Sync(Box<dyn FnOnce() -> ()>),
 }
--- src/thread_control/service/thread_worker_service.rs
+++ src/thread_control/service/thread_worker_service.rs
@@ -5,8 +5,14 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait ThreadWorkerServiceTrait {
-    fn save_async_thread_worker(&mut self, name: &str, will_be_execute_function: Arc<Mutex<dyn Fn() -> Pin<Box<dyn Future<Output = ()>>> + Send>>);
-    fn save_sync_thread_worker(&mut self, name: &str, will_be_execute_function: Arc<Mutex<dyn Fn() -> Pin<Box<dyn Future<Output = ()>>> + Send>>);
+    // fn save_async_thread_worker(&mut self, name: &str, will_be_execute_function: Arc<Mutex<dyn Fn() -> Pin<Box<dyn Future<Output = ()>>> + Send>>);
+    fn save_async_thread_worker<F>(&mut self, name: &str, will_be_execute_function: F)
+        where
+            F: FnOnce() -> Pin<Box<dyn Future<Output = ()> + Send>> + 'static;
+    // fn save_sync_thread_worker(&mut self, name: &str, will_be_execute_function: Arc<Mutex<dyn Fn() -> Pin<Box<dyn Future<Output = ()>>> + Send>>);
+    fn save_sync_thread_worker<F>(&mut self, name: &str, will_be_execute_function: F)
+        where
+            F: FnOnce() -> () + 'static;
     async fn start_thread_worker(&self, name: &str);
 }
 
